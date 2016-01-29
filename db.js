73230a90ac70e0f4ea161ad03150d6d052b1e03b
paulief <paulfagan1991@gmail.com>
2016-01-28 23:42:50 -0800

    Add probably useless db file for now

---
+++ db.js
@@ -0,0 +1,60 @@
+/*
+
+PROBABLY WILL DELETE THIS FILE, WAS GONNA USE IT FOR MIGRATIONS
+
+*/
+
+var Q 				 = require('q');
+var pg         = require('pg');
+
+var config     = require('config');
+var dbConfig   = config.get('Bump.dbConfig');
+var connString = buildConnString(dbConfig);
+process.env.DATABASE_URL = connString;
+
+var db = {};
+
+db.createTable = function(queryString) {
+	var deferred = Q.defer();
+
+	// TODO: get actual error messages
+	pg.connect(connString, function(err, client, done) {
+		if (err) {
+			console.log(err);
+			deferred.reject({
+				error: 'Connection Error',
+				message: 'Could not connect to DB'
+			});
+		} else {
+			client.query(queryString, function(err) {
+				if (err) {
+					console.log(err);
+					deferred.reject({
+						error: 'Query Error',
+						message: 'Could not execute query'
+					});
+				} else { //succesfully created
+					console.log('success');
+					deferred.resolve('Table successfully created');
+				}
+			});
+		}
+	});
+};
+
+module.exports = db;
+
+function buildConnString(dbConfig) {
+	return [
+		'postgres://',
+		dbConfig.username,
+		':',
+		dbConfig.password,
+		'@',
+		dbConfig.host,
+		':',
+		dbConfig.port,
+		'/',
+		dbConfig.name
+	].join('');
+}
