881143be7c6f2f8550605fb36edc6b7d6b1ccc20
paulief <paulfagan1991@gmail.com>
2016-01-31 14:42:55 -0800

    Create initial user model file

--- models/user.js
+++ models/user.js
@@ -1,1 +1,12 @@
-// Export some model methods
+var bookshelf = require('../helpers/bookshelf');
+
+import { Playlist } from 'models/playlist';
+
+var User = bookshelf.Model.extend({
+	tableName: 'users',
+	playlists: function() {
+		return this.hasMany(Playlist);
+	}
+});
+
+module.exports = User;
